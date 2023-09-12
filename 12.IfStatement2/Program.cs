9d70bdf75cc1defe4bb399a9d26074c327229624
Mabala-Karabo <mabalakarabo25@gmail.com>
2023-09-12 18:24:29 +0200

    Add project files.

---
+++ 12.IfStatement2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _12.IfStatement2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine(GetMax(2, 10));
+            Console.ReadLine();
+
+        }
+        static int GetMax(int num1, int num2)
+        {
+            //Console.BackgroundColor = ConsoleColor.Green;
+            Console.ForegroundColor = ConsoleColor.Blue; //making text blue
+            Console.Write("The maximum between " + num1 + " and " + num2 + " is: ");
+            if (num1 > num2)
+            {
+                return num1;
+            }
+            else
+            {
+                return num2;
+            }
+        }
+    }
+}
